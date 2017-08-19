108638c714ae2a2a045a79558a2da07d0ea2153f
Stephen A Smith <ssmit@udel.edu>
2017-08-19 18:01:14 -0400

    Added data models and sample data
    Added data models
    added sample data
    added celegans db
    added dbs to django

--- interactive_comp/icomp/models.py
+++ interactive_comp/icomp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class SamInput(models.Model):
+	sequence_string = models.CharField(max_length=256)
+	sequence_count = models.IntegerField(default=0)
+	sam_flag = models.IntegerField(default=0)
+	gene = models.CharField(max_length=128)
+
+class SamOutput(models.Model):
+	sequence_count_out = models.IntegerField(default=0)
+	gene_out = models.CharField(max_length=128)
+	noMatch1_counts = models.IntegerField(default=0)
+	noMatch2_counts = models.IntegerField(default=0)
+	match_count = models.IntegerField(default=0)
+
